9bedce4e4dc4eb632dd524e01f8937b142e2a859
atiqullah-naemi <atiqullah.naemi21@gmail.com>
2019-02-16 14:15:36 +1100

    creating components and fetching the api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react'
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from '../../config'
+import HeroImage from '../elements/HeroImage/HeroImage'
+import SearchBar from '../elements/SearchBar/SearchBar'
+import FourColGrid from '../elements/FourColGrid/FourColGrid'
+import MovieThumb from '../elements/MovieThumb/MovieThumb'
+import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn'
+import Spinner from '../elements/Spinner/Spinner'
+import './Home.css'
 
 class Home extends Component {
-   render() {
-      return (
-         <div className="Home">
-            React
-         </div>
-      )
-   }
+	state = {
+		movies: [],
+		heroImage: null,
+		loading: false,
+		currentPage: 0,
+		totalPages: 0,
+		searchTerm: ''
+	}
+
+	componentDidMount() {
+		this.setState({ loading: true })
+
+		const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+
+		this.getMovies(endpoint)
+	}
+
+	getMovies = (endpoint) => {
+		fetch(endpoint)
+			.then(res => res.json())
+			.then(data => {
+				this.setState({
+					movies: [...this.state.movies, ...data.results],
+					heroImage: this.state.heroImage || data.results[0],
+					loading: false,
+					currentPage: data.page,
+					totalPages: data.total_pages
+				})
+			})
+	}
+
+	loadMore = () => {
+		let endpoint = ''
+
+		this.setState({ loading: true })
+
+		if (this.state.searchTerm === '') {
+			endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`
+		} else {
+			endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`
+		}
+	}
+
+	render() {
+		return (
+			<div className="rmdb-home">
+				<HeroImage />
+				<SearchBar />
+				<FourColGrid />
+				<Spinner />
+				<LoadMoreBtn />
+			</div>
+		)
+	}
 }
 
 export default Home
