9a0b3c936014cc2073a42a2afaf1f7eeb5ff2bb3
atiqullah-naemi <atiqullah.naemi21@gmail.com>
2019-02-16 12:06:45 +1100

    creating movies app with react

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Header from '../elements/Header/Header'
+import Home from '../Home/Home'
+
+class App extends Component {
+   render() {
+      return (
+         <>
+            <Header />
+      		<Home />
+         </>
+      )
+   }
+}
+
+export default App
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
-import './App.css'
 
-class App extends Component {
+class Home extends Component {
    render() {
       return (
-         <div className="App">
+         <div className="Home">
             React
          </div>
       )
    }
 }
 
-export default App
+export default Home
